f5d94809bd0204186809e8342e86ab11e105dfd5
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-06-07 17:02:40 +0300

    add NavBar Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
+
+import { BrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div>
-     App
-    </div>
+    <BrowserRouter>
+      <NavBar />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components';
+import { NavLink as Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
+import logo from '../assets/logo.png';
+import shoppingCart from '../assets/shopping.png';
+
+const NavBar = () => (
+  <Nav>
+    <NavLink to="/products"><img alt="logo" src={logo} width="80px" height="80%" /></NavLink>
+    <NavMenu>
+      <NavLink to="/products" activeStyle><h1>Products</h1></NavLink>
+      <NavLink to="/favorites" activeStyle><h1>Favorites</h1></NavLink>
+      <NavLink to="/contact" activeStyle><h1>Contact</h1></NavLink>
+    </NavMenu>
+    <NavLink to="/cart" activeStyle>
+      <img alt="logo" style={{ color: 'white' }} src={shoppingCart} width="80px" height="80%" />
+    </NavLink>
+    <Bars />
+  </Nav>
+);
+export default NavBar;
+
+const Nav = styled.nav`
+  background: #000;
+  display: flex;
+  text-align: center;
+  justify-content: space-between;
+  height: 6rem;
+  font-size: 20px;
+  padding:0 12vw;
+  
+  @media screen and (max-width: 768px){
+    padding: 0;
+  } 
+`;
+const NavMenu = styled.nav`
+  display: flex;
+  align-items: center;
+`;
+
+const NavLink = styled(Link)`
+  color: white;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  
+  &.active {
+    color:aquamarine ;
+  }
+  transition: all 0.2s ease-in-out;
+  @media screen and (max-width: 768px){
+      display: ${(({ activeStyle }) => (activeStyle ? 'none' : null))}
+  }
+`;
+const Bars = styled(FaBars)`
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  display: none;
+  color: #fff;
+  @media screen and (max-width: 768px) {
+    display: flex;
+  }
+  width: 2.5rem;
+  height: 100%;
+  margin-right: 20px;
+`;
