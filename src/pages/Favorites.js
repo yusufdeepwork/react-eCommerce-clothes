ce0cf6cb3e2444fa53fe2db9380afeac3086f203
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-06-10 21:58:36 +0300

    add description for empty favorite page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,15 +1,35 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 import { ProductsContainer } from './Products';
 import { ProductContext } from '../context/ProductContext';
 import ProductItem from '../components/ProductItem';
 
 const Favorites = () => {
   const { favorites } = useContext(ProductContext);
-
   return (
-    <ProductsContainer>
-      {favorites.map((favorite) => <ProductItem product={favorite} favorite />)}
-    </ProductsContainer>
+    favorites.length === 0 ? (
+      <NoFavorite>
+        <text>You Dont have yet favorite Product.</text>
+        <text>You can look products. We are sure you will love.</text>
+      </NoFavorite>
+    ) : (
+      <ProductsContainer>
+        {favorites.map((favorite) => <ProductItem product={favorite} favorite />)}
+      </ProductsContainer>
+    )
+
   );
 };
 export default Favorites;
+const NoFavorite = styled.div`
+  font-family: "Fira Code Medium",monospace;
+  display: flex;
+  height: 100vh;
+  font-size:50px;
+  font-weight: bold;
+  justify-content: space-evenly;
+  align-items: center;
+  color: black;
+  text-align: center;
+  flex-direction: column;
+`;
